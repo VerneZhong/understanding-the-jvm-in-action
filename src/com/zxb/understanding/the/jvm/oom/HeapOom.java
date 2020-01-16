4d4fd210675cfa9f9b554f4debfa0563bbcf032b
Mr.zxb <zhongxuebin171980062@gmail.com>
2020-01-16 14:55:54 +0800

    深入理解Jvm虚拟机之Java Heap 溢出案例

--- src/com/zxb/understanding/the/jvm/oom/HeapOom.java
+++ src/com/zxb/understanding/the/jvm/oom/HeapOom.java
@@ -1,10 +1,15 @@
 package com.zxb.understanding.the.jvm.oom;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * {@link OutOfMemoryError} 异常示例
+ * {@link OutOfMemoryError} 异常示例，Java Heap 溢出 Java heap space
+ * Heap 大小 20m，并不可扩展
+ * -XX:+HeapDumpOnOutOfMemoryError 当出现溢出dump出异常快照
+ * -XX:HeapDumpPath=/usr/local/base  导出 dump oom 异常信息
+ * jmap -dump:file=/Users/zhongxuebin/jvm/dump/HeapOom.bin 47768 导出堆栈信息
  * VM Args: -verbose:gc -Xms20m -Xmx20m -Xmn10m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:SurvivorRatio=8
  *
  * @author Mr.zxb
@@ -16,7 +21,7 @@ public class HeapOom {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         List<OomObject> list = new ArrayList<>();
 
